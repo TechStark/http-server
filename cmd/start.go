2b994052ddd1fecfd1603cd0753dd0debb0db855
Wilson Tian <wilson.tian@perkinelmer.com>
2019-11-08 10:54:46 +0800

    add start command

---
+++ cmd/start.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var folder string
+var port int
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+	startCmd.Flags().StringVarP(&folder, "directory", "d", "", "Path of the web folder")
+	startCmd.Flags().IntVarP(&port, "port", "p", 8080, "Port number")
+}
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start the HTTP server",
+
+	Args: cobra.MinimumNArgs(0),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serving directory: ", folder)
+		fmt.Println("listening on port: ", port)
+		fileHandler := http.FileServer(http.Dir(folder))
+		http.ListenAndServe(fmt.Sprint(":", port), fileHandler)
+	},
+}
